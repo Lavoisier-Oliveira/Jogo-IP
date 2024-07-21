7a8637e500ec12f93b78d12f0ef70bc1e0afcb41
julia-zovka <jzs@cin.ufpe.br>
2024-07-21 19:09:20 -0300

    começo da classe de inimigos

--- entities/inimigos.py
+++ entities/inimigos.py
@@ -1,1 +1,10 @@
 import pygame
+import random
+
+Class Enemy():
+	def __init__(self, choice_inimigos):
+		self.image = pygame.image.load(f"assets/inimigos/Hull_0{choice_inimigos}.png") 
+		self.image = pygame.transform.scale(self.image, (50,50))  # Redimensiona a imagem do tanque
+		self.player_speed = 1
+		self.rect = self.image.get_rect(center=self.pos)
+    self.rect.center = (random.randint(30, 370), 0)
