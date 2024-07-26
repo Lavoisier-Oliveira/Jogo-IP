8cbea59d1a4f7f2b6f77ce41bc156985f37586dd
julia-zovka <jzs@cin.ufpe.br>
2024-07-26 14:40:44 -0300

    mudança vida, falta corrigir

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import sys
 from parameters import *
 from entities.tank import Tank
 from random import randint
+from entities.vida import Engrenagem
 
 # PyGame Setup
 pygame.init()
@@ -13,6 +14,9 @@ background_image = pygame.image.load(R'assets\backgrounds\back2.jpg')  # Load th
 background_image = pygame.transform.scale(background_image, (monitor.current_w, monitor.current_h))  # Resize the background image to fit the screen
 clock = pygame.time.Clock()
 
+engrenagem = Engrenagem(monitor.current_w, monitor.current_h)
+engrenagem_vezes=1
+
 p1 = Tank('A', randint(1, 10), [100, 100], 40, 15, KEYS_PLAYER_1)
 p2 = Tank('B', randint(1, 10), [200, 200], 50, 11, KEYS_PLAYER_2)
 p3 = Tank('C', randint(1, 10), [300, 300], 60, 10, KEYS_PLAYER_3)
@@ -20,6 +24,7 @@ p4 = Tank('D', randint(1, 10), [400, 400], 90, 7, KEYS_PLAYER_4)
 
 game_is_running = True
 while game_is_running:
+	game_time = pygame.time.get_ticks()
 	# Poll for events
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):  # para sair, pressione o X da janela ou ESC
@@ -31,6 +36,12 @@ while game_is_running:
 	for player in Tank.tanks:
 		player.update()
 
+ 
+	if game_time > 10000*engrenagem_vezes:
+		engrenagem.desenhar_engrenagem(screen)
+		engrenagem_vezes += 1
+		
+ 
 	# Renderizar o jogo
 	# screen.fill("black")  # Preencher a tela com uma cor (preto)
 	for player in Tank.tanks:
