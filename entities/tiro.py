3ccbfbd7b68d6176cdb50a4ac7e3254f84765114
Luis Eduardo Ribeiro Freitas <lerf@WINDOWS.CIN.UFPE.BR>
2024-07-31 09:41:34 -0300

    prototipo do tiro

--- entities/tiro.py
+++ entities/tiro.py
@@ -2,23 +2,24 @@ import pygame
 
 
 class Tiro():
-    def __init__(self, posicao_do_tanque, imagem, tamanho: tuple, posicao_do_tanque_atual=[]):
+    def __init__(self, posicao_do_tanque, tamanho: tuple, posicao_do_tanque_atual=[]):
         self.posicao_do_tanque = list(posicao_do_tanque)
-        self.imagem = imagem
+        self.imagem = pygame.image.load('assets\Effects\Medium_Shell.png')
         self.tamanho = tamanho
         self.velocidade = 1
         self.dano = 5
         self.posicao_do_tanque_atual = [0, 0]
-        self.rect = self.imagem.get_rect(center=posicao_do_tanque)
+        self.rect = self.imagem.get_rect(center=(posicao_do_tanque[0], posicao_do_tanque[1]))
 
-    # Função que renderiza a imagem da munição a partir da posição em que o tanque está
+    # Função que renderiza o tiro na tela
     def render(self, display):
-        self.posicao_do_tanque_atual[0], self.posicao_do_tanque_atual[1] = (80, self.posicao_do_tanque[0] - 80), (80, self.posicao_do_tanque[1] - 80)
-        display.blit(self.imagem, (self.posicao_do_tanque_atual[0], self.posicao_do_tanque_atual[1]))
+        display.blit(self.imagem, self.rect)
 
-    # Função que guarda a posição atual da munição, e continua renderizando ela na tela
-    def update(self, display):
-        display.blit(self.imagem, (self.posicao_do_tanque_atual[0], self.posicao_do_tanque_atual[1]))
-
-    def rect_municao(self):
-        return pygame.Rect(self.posicao_do_tanque_atual[0], self.posicao_do_tanque_atual[1], self.tamanho[0], self.tamanho[1])
+    def update(self, anglo):
+        if anglo == 0 or anglo == 180 or anglo == 360:
+            self.rect.x += self.velocidade
+        elif anglo == 90 or anglo == 270:
+            self.rect.y += self.velocidade
+        else:
+            self.rect.x += self.velocidade
+            self.rect.y += self.velocidade
